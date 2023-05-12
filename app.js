3620c1b20f5b50d86dd43d2bde9c0aaa9e1266e2
masperber <michael.sperber@automattic.com>
2023-05-12 16:03:49 -0400

    Implemented Rules and Computer Turn
    Implemented rules restricting player to removing pearls in only one row.
    
    Implemented random computer turn and game reset.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let gameState = {
   rows: [],
   playerTurn: true,
   gameOver: false,
+  rowSelected: -1 // the player may remove pearls from any row
 };
 
 function isPlayerTurn() {
@@ -19,8 +20,8 @@ function isPlayerTurn() {
 
 function countPearls() {
     let count = 0;
-    for (let i = 0; i < gameState.rows[i]; i++) {
-      count += gameState.rows[i].length;
+    for (let i = 0; i < gameState.rows.length; i++) {
+        count += gameState.rows[i];
     }
     return count;
 }
@@ -28,141 +29,126 @@ function countPearls() {
 
 // Generate a random number of rows with a random number of pearls in each row
 function generateGame() {
-  const numRows = Math.floor(Math.random() * (MAX_ROWS - MIN_ROWS + 1)) + MIN_ROWS; // generate between MIN_ROWS and MAX_ROWS rows
-  for (let i = 0; i < numRows; i++) {
-    const numPearls = Math.floor(Math.random() * (MAX_PEARLS - MIN_PEARLS + 1)) + MIN_PEARLS; // generate between 1 and 12 pearls per row
-    gameState.rows.push(numPearls);
-  }
-  gameState.rows.sort(function(a,b){return a-b});
+    gameState.rows = [];
+    gameState.playerTurn = true;
+    gameState.rowSelected = -1;
+    const numRows = Math.floor(Math.random() * (MAX_ROWS - MIN_ROWS + 1)) + MIN_ROWS; // generate between MIN_ROWS and MAX_ROWS rows
+    for (let i = 0; i < numRows; i++) {
+        const numPearls = Math.floor(Math.random() * (MAX_PEARLS - MIN_PEARLS + 1)) + MIN_PEARLS; // generate between 1 and 12 pearls per row
+        gameState.rows.push(numPearls);
+    }
+    gameState.rows.sort(function(a,b){return a-b});
+    console.log(gameState.rows);
+    console.log(countPearls());
 }
 
 function updateMessage(message) {
-  messageBox.textContent = message;
+    messageBox.textContent = message;
 }
 
 function endTurn() {
-    // If there is only one pearl remaining, end the game
-    if (countPearls() === 1) {
-        endGame();
+    // If it is the player's turn, switch to the AI's turn
+    if (isPlayerTurn()) {
+        gameState.playerTurn = false;
+        gameState.rowSelected = -1;
+        updateMessage("It is now the computer's turn.");
+        setTimeout(handleComputerTurn, 1000); // wait 1 second before making the move
     }
-
+    // If it is the AI's turn, switch to the player's turn
     else {
-        // If it is the player's turn, switch to the AI's turn
-        if (isPlayerTurn()) {
-            gameState.playerTurn = false;
-            updateMessage("It is now the computer's turn.");
-            setTimeout(handleComputerTurn, 1000); // wait 1 second before making the move
-        }
-        // If it is the AI's turn, switch to the player's turn
-        else {
-            gameState.playerTurn = true;
-            updateMessage("It is now your turn.");
-        }
+        gameState.playerTurn = true;
+        gameState.rowSelected = -1;
+        updateMessage("It is now your turn.");
     }
 }  
 
 // Render the game board based on the current game state
 function renderGame() {
-  gameContainer.innerHTML = "";
-  for (let i = 0; i < gameState.rows.length; i++) {
-    const rowContainer = document.createElement("div");
-    rowContainer.classList.add("game-row");
-    for (let j = 0; j < gameState.rows[i]; j++) {
-      const pearl = document.createElement("div");
-      pearl.classList.add("pearl");
-      pearl.addEventListener("click", () => handlePearlClick(i)); // add a click event listener to each pearl
-      rowContainer.appendChild(pearl);
+    gameContainer.innerHTML = "";
+    for (let i = 0; i < gameState.rows.length; i++) {
+            const rowContainer = document.createElement("div");
+            rowContainer.classList.add("game-row");
+            for (let j = 0; j < gameState.rows[i]; j++) {
+                const pearl = document.createElement("div");
+                pearl.classList.add("pearl");
+                pearl.addEventListener("click", () => handlePearlClick(i)); // add a click event listener to each pearl
+                rowContainer.appendChild(pearl);
+            }
+        gameContainer.appendChild(rowContainer);
     }
-    gameContainer.appendChild(rowContainer);
-  }
+}
+
+function removePearl(row) {
+    gameState.rows[row]--; // remove the clicked pearl
+    console.log(gameState.rows);
+    console.log(countPearls());
+    // if the row is empty, remove it from the game state
+    if (gameState.rows[row] == 0) {
+        gameState.rows.splice(row,1);
+        gameState.rowSelected = -2; // the player may not remove any more pearls
+    }
+    renderGame(); // re-render the game board to reflect the updated state
 }
 
 // Handle when the player clicks on a pearl
-function handlePearlClick(rowIndex) {
-  if (!isPlayerTurn() || gameState.gameOver ) {
-    return; // ignore clicks if it's not the player's turn or the game is over
-  }
-  gameState.rows[rowIndex]--; // remove the clicked pearl
-  renderGame(); // re-render the game board to reflect the updated state
-  endTurnButton.classList.remove("disabled"); // enable the "end turn" button
-}
-/*
-// Handle when the player clicks the "end turn" button
-function handleEndTurn() {
-  if (!isPlayerTurn() || gameState.gameOver) {
-    return; // ignore clicks if it's not the player's turn or the game is over
-  }
-  const endTurnButton = document.getElementById("end-turn-button");
-  endTurnButton.classList.add("disabled"); // disable the "end turn" button
-  gameState.isPlayerTurn = false; // switch to the computer's turn
-  renderGame(); // re-render the game board to reflect the updated state
-  setTimeout(() => handleComputerTurn(), 500); // add a small delay to simulate the computer "thinking"
+function handlePearlClick(row) {
+    if (!isPlayerTurn() || gameState.gameOver || (gameState.rowSelected != -1 && gameState.rowSelected != row)) {
+        return; // ignore clicks if it's not the player's turn or the game is over
+    }
+    gameState.rowSelected = row;
+    removePearl(row);
+    if (countPearls() == 1) {
+        endGame(isPlayerTurn());
+    }
+    endTurnButton.classList.remove("disabled"); // enable the "end turn" button
 }
 
 function handleComputerTurn() {
-  // generate a random row
-  let row = Math.floor(Math.random() * gameState.rows.length);
+    // generate a random row
+    let row = Math.floor(Math.random() * gameState.rows.length);
+  
+    // generate a random number of pearls to remove from the row
+    let numToRemove = Math.floor(Math.random() * (gameState.rows[row] - 1)) + 1;
+    
+    // remove the pearls from the row
+    for (let i = 0; i < numToRemove; i++) {
+        removePearl(row);
+    }
 
-  // generate a random number of circles to remove from the row
-  let numToRemove = Math.floor(Math.random() * (numCircles[row] - 1)) + 1;
-
-  // remove the circles from the row
-  for (let i = 0; i < numToRemove; i++) {
-    let circle = document.getElementById(`circle-${row}-${numCircles[row]-i}`);
-    circle.style.display = "none";
-  }
-
-  // check if the player won
-  if (checkWin()) {
-    endGame(false);
-  } else {
-    // switch back to the player's turn
-    gameState.isPlayerTurn = true;
-    updateMessage("Your turn");
-    endTurnButton.disabled = false;
-  }
-}
-
-function checkWin() {
-  let numCirclesLeft = 0;
-
-  // loop through each row and count the number of circles left
-  for (let i = 0; i < numRows; i++) {
-    let rowCircles = document.querySelectorAll(`.circle-row-${i}`);
-    numCirclesLeft += rowCircles.length;
-  }
-
-  // return true if only one circle is left
-  return numCirclesLeft === 1;
+    if (countPearls() == 1) {
+        endGame(isPlayerTurn());
+    }
+    else {
+        endTurn();
+    }
 }
 
 function newGame() {
     // Reset game state
-    board = generateGame();
-    gameState.isPlayerTurn = true;
-  
+    generateGame();
+    renderGame();
+
     // Update message and UI
     updateMessage("New game started. Your turn.");
-    renderGameBoard();
-  
-    // Reset endTurn button
-    endTurnButton.removeEventListener("click", newGame());
-    endTurnButton.addEventListener("click", endTurn());
+    
+    // Reset end turn button
+    endTurnButton.removeEventListener("click", newGame);
+    endTurnButton.addEventListener("click", endTurn);
     endTurnButton.textContent = "End Turn";
-  }
-  
+}
 
 function endGame(playerWon) {
-  if (playerWon) {
-    updateMessage("You won!");
-  } else {
-    updateMessage("Computer won!");
-  }
-  endTurnButton.innerHTML = "New game";
-  endTurnButton.removeEventListener("click", endTurn());
-  endTurnButton.addEventListener("click", newGame());
+    gameState.gameOver = true;
+    if (playerWon) {
+        updateMessage("You won!");
+    } else {
+        updateMessage("Computer won!");
+    }
+    endTurnButton.textContent = "New game";
+    endTurnButton.removeEventListener("click", endTurn);
+    endTurnButton.addEventListener("click", newGame);
 }
-*/
+
 window.addEventListener("load", function () {
   // generate a new game board
   generateGame();
@@ -170,7 +156,7 @@ window.addEventListener("load", function () {
 
   // initialize the turn variables
   updateMessage(isPlayerTurn() ? "Your turn" : "Computer's turn");
-/*
-  // add event listeners to the buttons
-  endTurnButton.addEventListener("click", endTurn());*/
+
+  // add event listener to the end turn button
+  endTurnButton.addEventListener("click", endTurn);
 });
